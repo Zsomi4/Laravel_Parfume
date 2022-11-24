410b658f6ea3ad31dc1a0b3265c53982c8a255d3
Zsomi4 <k72302872172@ktch.hu>
2022-11-24 11:06:35 +0100

    Bootstrap hozzáadása.

--- resources/views/edit_perfume.blade.php
+++ resources/views/edit_perfume.blade.php
@@ -1,3 +1,5 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 <title>Szerkesztés</title>
 
 @extends( "layouts.master" )
@@ -30,7 +32,7 @@
         <input type="text" name="price" value="{{ $perfume->price }}">
     </p>
     <p>
-        <button type="submit">Küldés</button>
+        <button type="submit" class="btn btn-success">Küldés</button>
     </p>
 </form>
 @endsection
